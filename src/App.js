db34ccdbbabc1d2a7ba33a1d32b52d77977ebedd
siamahmd <si4m4hmed@gmail.com>
2023-09-24 16:26:07 +0600

    Implemented Routes and UI Updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
-import Login from "./components/Login";
+import Header from "./components/Header";
+import FrontPage from "./containers/FrontPage";
+import Todos from "./components/Todos";
+import { Route, Link, Routes } from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  background: green;
+  height: 91.5vh;
+  display: flex;
+`;
+
+const NavWrapper = styled.div`
+  background: red;
+  flex: 0 0 20%;
+  height: 100%;
+  padding: 20px 30px;
+`;
+
+const TodosWrapper = styled.div`
+  background: yellow;
+  flex: 0 0 80%;
+  height: 100%;
+  padding: 20px 30px;
+  overflow-x: hidden;
+  overflow-y: auto;
+`;
 
 function App() {
-  return <Login></Login>;
+  return (
+    <>
+      <Header></Header>
+      <Wrapper>
+        <NavWrapper>
+          <nav>
+            <ul>
+              <Link to="/">Home</Link>
+              <Link to="/todos">Todos</Link>
+            </ul>
+          </nav>
+        </NavWrapper>
+        <TodosWrapper>
+          <Routes>
+            <Route exact path="/" element={<FrontPage />} />
+            <Route path="todos" element={<Todos />} />
+          </Routes>
+        </TodosWrapper>
+      </Wrapper>
+    </>
+  );
 }
 
 export default App;
---
+++ src/containers/FrontPage.js
@@ -0,0 +1,7 @@
+export default function FrontPage() {
+  return (
+    <>
+      <h1>New Todo</h1>
+    </>
+  );
+}
